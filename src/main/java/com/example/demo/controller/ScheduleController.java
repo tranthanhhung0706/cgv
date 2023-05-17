c392c38b2de7a308fa8588526189ca243a8b03cd
Phuong Nguyen <phuongsover2@gmail.com>
2023-05-17 22:10:32 +0700

    kiểm tra thêm room trước khi chuyển sang DTO

--- src/main/java/com/example/demo/controller/ScheduleController.java
+++ src/main/java/com/example/demo/controller/ScheduleController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.RoomDTO;
 import com.example.demo.dto.ScheduleDTO;
 import com.example.demo.dto.SeatDTO;
 import com.example.demo.dto.ShowScheduleDTO;
@@ -67,6 +68,9 @@ public class ScheduleController {
     public ScheduleDTO getScheduleById(@RequestParam("id") int id) {
         Schedule schedule = scheduleRepository.findById(id).orElse(null);
         ScheduleDTO scheduleDTO = new ScheduleDTO(schedule);
+        if (schedule.getRoom() != null) {
+            scheduleDTO = new ScheduleDTO(schedule, new RoomDTO(schedule.getRoom()));
+        }
 
         List<Ticket> tickets = ticketRepository.findBySchedule(schedule);
         List<String> seats = new ArrayList<>();
@@ -79,10 +83,10 @@ public class ScheduleController {
         return scheduleDTO;
     }
 
-    @GetMapping("/schedule")
-    public List<ScheduleDTO> getSchedule() {
-        return scheduleService.getAllSchedule();
-    }
+    // @GetMapping("/schedule")
+    // public List<ScheduleDTO> getSchedule() {
+    // return scheduleService.getAllSchedule();
+    // }
 
     @PostMapping("/schedule")
     public ScheduleDTO newSchedule(@RequestBody ScheduleDTO scheduleDTO) {
